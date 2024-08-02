c4df900b6ef09c6bbe2650027f9e5e37d06688ab
Andrew <127788127788a@gmail.com>
2024-08-02 23:03:08 +0300

    tenth lesson cpp

--- cppLessons/main.cpp
+++ cppLessons/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
 
 int main() {
-   
-    std::cout << "Hello, World!\n";
-    int a;
-    std::cout << "A: ";
-    std::cin >>  a;
-    std::cout << a << "\n";
+    int *nums = new int[3];
+    nums[0] = 45;
+    cout << nums[0] << endl;
+    delete[] nums;
+    
+    char word[] = {'H', 'i'};
+    for(int i = 0; i < 2; i++){
+        cout << word[i];
+    }
+    
+    string words = "Hello world!";
+    cout << "\n" << words << endl;
+    
+    cout << "New: ";
+    cin >> words;
+    cout << words << endl;
+    
+    
     return 0;
 }
