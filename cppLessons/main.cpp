7bc5cf6a82a02cb4ed76027b5497064e5e2e8f23
Andrew <127788127788a@gmail.com>
2024-08-10 22:32:47 +0300

    created library

--- cppLessons/main.cpp
+++ cppLessons/main.cpp
@@ -1,40 +1,110 @@
 #include "iostream"
 #include "string"
+#include <vector>
 
 using namespace std;
 
-class PC {
+
+class Author {
 private:
-    int diagonal;
-    string os;
+    string name;
+    bool isAlive;
+    int age;
 public:
-    PC(int diagonal, string os){
-        this->diagonal = diagonal;
-        this->os = os;
+    Author(string name, bool isAlive, int age){
+        this->name = name;
+        this->isAlive = isAlive;
+        this->age = age;
     }
     void get_info(){
-        cout << "OS: " << os << " Diagonal: " << diagonal;
+        cout << "Name: " << name << ". Alive: " << (isAlive ? "Yes" : "No") << ". Age: " << age << endl;
     }
 };
 
-class Laptop: public PC{
+class Book {
 private:
-    float weight;
+    string title;
+    string manufacture_year;
+    string genre;
+    Author author;
+    bool isBorrowed = false;
 public:
-    Laptop(int diagonal, string os, float weight): PC(diagonal, os){
-        this->weight = weight;
-    }
+    Book(string title, string manufacture_year, string genre, Author author)
+       : title(title), manufacture_year(manufacture_year), genre(genre), author(author) {}
     void get_info(){
-        PC::get_info();
-        cout << ". Weight: " << weight << endl;
+        cout << "Book Title: " << title << ". Manufacture Year: " << manufacture_year << ". Genre: " << genre << endl;
+        cout << "About author: ";
+        author.get_info();
+    }
+    bool getBorrowedStatus() const {
+        return isBorrowed;
+    }
+    string getTitle() const {
+        return title;
+    }
+    void setBorrowedStatus(bool status){
+        isBorrowed = status;
+    }
+    void print() const {
+        cout << "Title: " << title << ", Borrowed: " << (isBorrowed ? "Yes" : "No") << endl;
+    }
+};
+
+class Library {
+private:
+    vector<Book> books;
+public:
+    void addBook(Book book){
+        books.push_back(book);
+    }
+    void borrowBook(const string& title) {
+        for (Book& book : books) {
+            if (book.getTitle() == title && !book.getBorrowedStatus()) {
+                book.setBorrowedStatus(true);
+                return;
+            }
+        }
+        cout << "Book not found or already borrowed." << endl;
+    }
+
+    void getBorrowedBookBack(const string& title) {
+        for (Book& book : books) {
+            if (book.getTitle() == title && book.getBorrowedStatus()) {
+                book.setBorrowedStatus(false);
+                return;
+            }
+        }
+        cout << "Book not found or not borrowed." << endl;
+    }
+
+    void getBooksList() const {
+        for (const auto& book : books) {
+            book.print();
+        }
     }
 };
 
 int main(){
     setlocale(LC_ALL, "RU");
-    
-    Laptop mac(16, "MacOS", 1.5f);
+    Author Ivan_Franko("Ivan Franko", false, 59);
+    Book zahar("Захар Беркут", "1883", "Фикшн", Ivan_Franko);
+    Book kamenyry("Каменярі", "1887", "Поэзия", Ivan_Franko);
+    Book mykyta("Лис Микита", "1879", "Сказка", Ivan_Franko);
+    Book borislav("Борислав сміється", "1887", "Драматургия", Ivan_Franko);
+    Book ivan("Іван Вишенський", "1883", "Роман", Ivan_Franko);
+    Book moysey("Мойсей", "1905", "Эпопея", Ivan_Franko);
    
-    mac.get_info();
+    Library library;
+    library.addBook(zahar);
+    library.addBook(kamenyry);
+    library.addBook(mykyta);
+    library.addBook(borislav);
+    library.addBook(ivan);
+    library.addBook(moysey);
+    
+    library.getBooksList();
+    library.borrowBook("Захар Беркут");
+    library.getBooksList();
+    
    
 }
